8d3bb61698c4415c24cfa5fced21b66d80e40855
Edward Joseph A. Habon <Edward.Habon@cebupacificair.com>
2024-06-19 13:43:43 +0800

    Add Sidebar (desktop)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Edward', lastName: 'EJH', email: 'testmail@mail.com'}
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
