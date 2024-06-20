36a912e99bc2da9730069d415d816689754d31d3
Edward Joseph A. Habon <Edward.Habon@cebupacificair.com>
2024-06-20 14:57:20 +0800

    Add Right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Edward'}
+  const loggedIn = {firstName: 'Edward', lastName: 'EJH', email: 'edward@test.com'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -11,7 +12,13 @@ const Home = () => {
           <HeaderBox type='greeting' title='Welcome' user={loggedIn.firstName || 'Guest'} subtext='Access and manage your and transactions efficiently.' />
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1234.56}, {currentBalance: 678.96}]}
+       />
     </section>
   )
 }
