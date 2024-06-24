1b96190cc122b2dafb9c93f624763d15627ad4d9
Edward Joseph A. Habon <Edward.Habon@cebupacificair.com>
2024-06-24 17:32:40 +0800

    Initial AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,10 +1,38 @@
 'use client';
 
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Form
+} from "@/components/ui/form";
+import { authFormSchema } from '@/lib/utils';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import CustomInput from './CustomInput';
 
 const AuthForm = ({type}:AuthFormProps) => {
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: "",
+      password:""
+    },
+  })
+  const {formState: {isLoading}} = form
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   const [user, setUser] = useState(null)
   return (
     <section className='auth-form'>
@@ -21,10 +49,59 @@ const AuthForm = ({type}:AuthFormProps) => {
 
         <div className='flex flex-col gap-1 md:gap-3'>
           <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
-            {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+            {user 
+              ? 'Link Account' 
+              : type === 'sign-in' 
+                ? 'Sign In' 
+                : 'Sign Up'}
+              <p className='text-16 font-normal text-gray-600'>
+                {user ? 'Link your account to get started' : 'Please enter your details'}
+              </p>
           </h1>
         </div>
       </header>
+      {user ? (
+        <div className='flex flex-col gap-4'>
+          {/* PlaidLink */}
+        </div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomInput 
+                control={form.control}
+                label="Email"
+                name="email"
+                placeholder="Enter your email"
+                 />
+              <CustomInput 
+                control={form.control} 
+                label="Password"
+                name="password"
+                placeholder="Enter your password"
+                 />
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ?
+                    <Loader2 size={20} className="animate-spin" />
+                    : type === 'sign-in'
+                    ? 'Sign In' : 'Sign Up'
+                  }
+                </Button>
+              </div>
+            </form>
+          </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14  font-normal text-gray-600">
+                {type === 'sign-in' ? "Don't have an account?" : "Already have an account?"}
+            </p>
+            <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
+            {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+            </Link>
+          </footer>
+        </>
+      )}
     </section>
   )
 }
---
+++ components/CustomInput.tsx
@@ -0,0 +1,40 @@
+import { Input } from "@/components/ui/input";
+import { authFormSchema } from '@/lib/utils';
+import { Control, FieldPath } from 'react-hook-form';
+import { z } from 'zod';
+import { FormControl, FormField, FormLabel, FormMessage } from './ui/form';
+
+interface Props {
+    control: Control<z.infer<typeof authFormSchema>>;
+    name: FieldPath<z.infer<typeof authFormSchema>>;
+    label: string;
+    placeholder: string;
+}
+
+const CustomInput = ({control, name, label, placeholder}:Props) => {
+  return (
+    <FormField
+    control={control}
+    name={name}
+    render={({ field }) => (
+      <div className='form-item'>
+        <FormLabel className='form-label'>
+          {label}
+        </FormLabel>
+        <div className='flex w-full flex-col'>
+          <FormControl>
+            <Input 
+              placeholder={placeholder}
+              className='input-class' 
+              {...field}
+              type={name === 'password' ? 'password' : 'text'} />
+          </FormControl>
+          <FormMessage className='form-message mt-2' />
+        </div>
+      </div>
+    )}
+  />
+  )
+}
+
+export default CustomInput
